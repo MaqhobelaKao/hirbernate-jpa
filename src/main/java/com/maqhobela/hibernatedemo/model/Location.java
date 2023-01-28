461b36dec3070e7793cacaf33050f4965633b60c
MaqhobelaKao <kaomaqhobela@gmail.com>
2023-01-28 16:05:32 +0200

    Added initilization data at boot time

--- src/main/java/com/maqhobela/hibernatedemo/model/Location.java
+++ src/main/java/com/maqhobela/hibernatedemo/model/Location.java
@@ -2,8 +2,10 @@ package com.maqhobela.hibernatedemo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "location")
 public class Location {
     @Id
     private Integer id;
