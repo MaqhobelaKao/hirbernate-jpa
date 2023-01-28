1ddc50605f0a5a0e967d3699122261661b204364
MaqhobelaKao <kaomaqhobela@gmail.com>
2023-01-28 18:30:38 +0200

    Created user and Location controllers for returning both a list of users or locations and user by id or location by id

--- src/main/java/com/maqhobela/hibernatedemo/controller/LocationController.java
+++ src/main/java/com/maqhobela/hibernatedemo/controller/LocationController.java
@@ -1,7 +1,29 @@
 package com.maqhobela.hibernatedemo.controller;
 
+import com.maqhobela.hibernatedemo.model.Location;
+import com.maqhobela.hibernatedemo.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LocationController {
+
+    @Autowired
+    LocationService locationService;
+
+    @GetMapping("/locations")
+    public List<Location> getAllLocoations(){
+        return locationService.getLocations();
+    }
+
+    @GetMapping("/location/{id}")
+    public Optional<Location> getAllLocoationById(@PathVariable("id") Integer id){
+        return locationService.getLocation(id);
+    }
+
 }
--- src/main/java/com/maqhobela/hibernatedemo/controller/UserController.java
+++ src/main/java/com/maqhobela/hibernatedemo/controller/UserController.java
@@ -1,7 +1,28 @@
 package com.maqhobela.hibernatedemo.controller;
 
+import com.maqhobela.hibernatedemo.model.User;
+import com.maqhobela.hibernatedemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/users")
+    public List<User> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> getUserById(@PathVariable(name = "id") Integer id){
+        return userService.getUser(id);
+    }
 }
