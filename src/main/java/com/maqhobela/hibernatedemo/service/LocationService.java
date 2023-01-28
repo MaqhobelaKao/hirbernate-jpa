a14c588fbad4117f6cd67906a710e1408b0861bc
MaqhobelaKao <kaomaqhobela@gmail.com>
2023-01-28 18:15:20 +0200

    implemented service that get all users and get one user by id as well as all locations and one location by id from database

--- src/main/java/com/maqhobela/hibernatedemo/service/LocationService.java
+++ src/main/java/com/maqhobela/hibernatedemo/service/LocationService.java
@@ -1,7 +1,23 @@
 package com.maqhobela.hibernatedemo.service;
 
+import com.maqhobela.hibernatedemo.model.Location;
+import com.maqhobela.hibernatedemo.repository.LocationRepository;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class LocationService {
+    @Resource
+    LocationRepository locationRepository;
+
+    public List<Location> getLocations(){
+        return (List<Location>) locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocation(Integer id){
+        return locationRepository.findById(id);
+    }
 }
--- src/main/java/com/maqhobela/hibernatedemo/service/UserService.java
+++ src/main/java/com/maqhobela/hibernatedemo/service/UserService.java
@@ -1,7 +1,26 @@
 package com.maqhobela.hibernatedemo.service;
 
+import com.maqhobela.hibernatedemo.model.User;
+import com.maqhobela.hibernatedemo.repository.UserRepository;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
+    @Resource
+    UserRepository userRepository;
+
+    public List<User> getAllUsers(){
+        return (List<User>) userRepository.findAll();
+    }
+
+
+    public Optional<User> getUser(Integer id){
+        return userRepository.findById(id);
+    }
 }
